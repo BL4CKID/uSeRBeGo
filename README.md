619075d824cac0f9a40bd5d0ae7bdc02227bffbf
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2020-10-07 14:01:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">One4uBot MOD by <a href="https://t.me/BL4CK_ID">𝐁𝐋𝟒𝐂𝐊_𝐈𝐃 </h1>
+<p align="center">&nbsp;</p>
+<h2 align="center">How To Host</h2>
+<h5 align="center">The easiest way to deploy this One4uBot! is click on the image below</h5>
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/BL4CKID/One4uBot/tree/master"> <img src="https://telegra.ph/file/34de47d00656e00c0f4e2.jpg" alt="Deploy to Heroku" /></a></p>
+<p align="center">&nbsp;</p>
+    
+<h2 align="center">NOTES</h2>
+<p align="center">Thanks to all CREATOR of userbot. Regards <a href="https://t.me/BL4CK_ID">𝐁𝐋𝟒𝐂𝐊_𝐈𝐃 </p>
+
+For further guide you can head to [Groups and Support](https://github.com/MoveAngel/One4uBot#Groups-and-Support) or read this [guide](https://telegra.ph/How-to-host-a-Telegram-Userbot-07-01-2)
+
+## Groups and Support
+
+if you want new features, or announcements, you can follow our [UserbotIndo Channel](https://t.me/userbotindocloud).
+
+For discussion, bug reporting, and help, you can join [UserbotIndo Support Group](https://t.me/userbotindo).
+
+## Credits
+
+Thanks: 
+* [MoveAngel](https://github.com/MoveAngel/One4uBot) - Source One4uBot
+* [RaphielGang](https://github.com/RaphielGang) - Telegram-Paperplane
+* [AvinashReddy3108](https://github.com/AvinashReddy3108) - PaperplaneExtended
+* [kandnub](https://github.com/kandnub) - TG-UserBot
+* [AdekMaulana](https://github.com/adekmaulana) - ProjectBish
+* [Mr.Miss](https://github.com/keselekpermen69) - Userbutt
+* [GengKapak](https://github.com/GengKapak) - DCLXVI
+* [Mkaraniya](https://github.com/mkaraniya) & [Dev73](https://github.com/Devp73) - OpenUserBot
+
+and many more people who aren't mentioned here, but may be found in [Contributors](https://github.com/MoveAngel/One4uBot/graphs/contributors).
+
+## License
+
+This userbot licensed on [Raphielscape Public License](https://github.com/MoveAngel/One4uBot/blob/sql-extended/LICENSE) - Version 1.d, February 2020
---
+++ userbot/modules/misc.py
@@ -0,0 +1,131 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You can find misc modules, which dont fit in anything xD
+""" Userbot module for other small commands. """
+
+import io
+import sys
+from os import execl
+from random import randint
+from time import sleep
+
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+from userbot.utils import time_formatter
+
+
+@register(outgoing=True, pattern="^.xrandom")
+async def randomise(items):
+    """ For .random command, get a random item from the list of items. """
+    itemo = (items.text[8:]).split()
+    if len(itemo) < 2:
+        await items.edit(
+            "`2 or more items are required! Check .help random for more info.`"
+        )
+        return
+    index = randint(1, len(itemo) - 1)
+    await items.edit(
+        "**Query: **\n`" + items.text[8:] + "`\n**Output: **\n`" + itemo[index] + "`"
+    )
+
+
+@register(outgoing=True, pattern="^.sleep ([0-9]+)$")
+async def sleepybot(time):
+    """ For .sleep command, let the userbot snooze for a few second. """
+    counter = int(time.pattern_match.group(1))
+    await time.edit("`I am sulking and snoozing...`")
+    if BOTLOG:
+        str_counter = time_formatter(counter)
+        await time.client.send_message(
+            BOTLOG_CHATID,
+            f"You put the bot to sleep for {str_counter}.",
+        )
+    sleep(counter)
+    await time.edit("`OK, I'm awake now.`")
+
+
+@register(outgoing=True, pattern="^.shutdown$")
+async def killbot(shut):
+    """For .shutdown command, shut the bot down."""
+    await shut.edit("Shutdown...")
+    if BOTLOG:
+        await shut.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
+    await bot.disconnect()
+
+
+@register(outgoing=True, pattern="^.restart$")
+async def killdabot(reboot):
+    await reboot.edit("Restarting...")
+    if BOTLOG:
+        await reboot.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted")
+    await bot.disconnect()
+    # Spin a new instance of bot
+    execl(sys.executable, sys.executable, *sys.argv)
+    # Shut the existing one down
+    exit()
+
+
+@register(outgoing=True, pattern="^.xreadme$")
+async def reedme(event):
+    await event.edit(
+        "Here's something for you to read:\n"
+        "\n[One4uBot's README.md file](https://github.com/MoveAngel/One4uBot/blob/sql-extended/README.md)"
+        "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-07-01-2)"
+        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
+        "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
+        "\n[Setup Guide - From MiHub with Pict](https://www.mihub.my.id/2020/05/jadiuserbot.html)"
+        "\n[Setup Guide - In Indonesian Language](https://telegra.ph/UserIndoBot-05-21-3)"
+        "\n[Instant Setup - Generate String Session](https://userbotsession.moveangel.repl.run)"
+    )
+
+
+# Copyright (c) Gegham Zakaryan | 2019
+@register(outgoing=True, pattern="^.xrepeat (.*)")
+async def repeat(rep):
+    cnt, txt = rep.pattern_match.group(1).split(" ", 1)
+    replyCount = int(cnt)
+    toBeRepeated = txt
+
+    replyText = toBeRepeated + "\n"
+
+    for i in range(0, replyCount - 1):
+        replyText += toBeRepeated + "\n"
+
+    await rep.edit(replyText)
+
+
+@register(outgoing=True, pattern="^.repo$")
+async def repo_is_here(wannasee):
+    """ For .repo command, just returns the repo URL. """
+    await wannasee.edit(
+        "[Click here](https://github.com/MoveAngel/One4uBot) to open One4uBot's GitHub page."
+    )
+
+
+@register(outgoing=True, pattern="^.xraw$")
+async def raw(rawtext):
+    the_real_message = None
+    reply_to_id = None
+    if rawtext.reply_to_msg_id:
+        previous_message = await rawtext.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = rawtext.reply_to_msg_id
+    else:
+        the_real_message = rawtext.stringify()
+        reply_to_id = rawtext.message.id
+    with io.BytesIO(str.encode(the_real_message)) as out_file:
+        out_file.name = "raw_message_data.txt"
+        await rawtext.edit("`Check the userbot log for the decoded message data !!`")
+        await rawtext.client.send_file(
+            BOTLOG_CHATID,
+            out_file,
+            force_document=True,
+            allow_cache=False,
+            reply_to=reply_to_id,
+            caption="`Here's the decoded message data !!`",
+        )
+
+
